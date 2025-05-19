1f9f95621fc9c9c7d06af28e67450f4c3540e121
unknown <lizokia8@gmail.com>
2025-05-19 10:08:05 +0300

    Temp commit before rebase

--- real-time-temperature-monitoring-for-animals/next.config.ts
+++ real-time-temperature-monitoring-for-animals/next.config.ts
@@ -1,15 +1,7 @@
-// filepath: c:\University\UPB\os2\project\Real-Time-Temperature-Monitoring-for-Animals\real-time-temperature-monitoring-for-animals\next.config.ts
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*', // Proxy to backend
-      },
-    ];
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Remove any "appDir: true"
 };
 
-export default nextConfig;
+module.exports = nextConfig;
